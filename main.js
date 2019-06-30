8d608204e42fa728102bd2981cca4fa8f7733255
jay214421343 <48416316+jay214421343@users.noreply.github.com>
2019-06-30 11:26:33 +0200

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const bot = new RhythmBot({
         symbol: '!' // command symbol trigger
     },
     discord: {
-        token: 'NDkzMDEwMTM3NjYxODMzMjM3.XOqMag.egN_wjR2Sl1w-JvetavnesV88ak'//,
+        token: ''//,
         // manage: {
         //     channels: [ 
         //         // Example text channel manager, limits media channel to 5 posts, limit should probably be less than 100 to avoid bugs
